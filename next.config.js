a1cec88469b541714169f9cc3a9b82a4d48b9c03
hwangTaehyeon <clowns1232@gmail.com>
2024-11-12 19:26:56 +0900

    test

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   output: "standalone",
   distDir: "out", // 빌드 파일을 `out` 디렉터리에 저장
+  basePath: "/clowns1232.github.io", // 리포지토리 이름으로 변경
+  assetPrefix: "/clowns1232.github.io", // 정적 자산 경로 설정
   webpack: (config) => {
     config.module.rules.push({
       test: /\.md/,
